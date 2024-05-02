562db221f7482260c2da10179b09d656a0905e38
Mateo A <mateoarcilatoro@gmail.com>
2024-05-01 20:17:19 -0500

    Removed hardcoded variables

--- src/main/java/co/com/eress/automation/constants/EressConstants.java
+++ src/main/java/co/com/eress/automation/constants/EressConstants.java
@@ -2,8 +2,8 @@ package co.com.eress.automation.constants;
 
 public class EressConstants {
     public static final String BASE_URL = "http://170.187.157.103:4200/";
-    public static final String EMAIL = "pruebasqa@enequipo.co";
-    public static final String PASSWORD = "Qa#$*2012";
+    public static final String EMAIL = System.getenv("email");
+    public static final String PASSWORD = System.getenv("pass");
 
     public static final String CONFECCIONES_WINDOW = "ERESS - CONFECCIÓN";
 }
